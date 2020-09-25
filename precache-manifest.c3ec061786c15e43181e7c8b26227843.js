95a985ad86f90298270c6732504d2f5758e39adb
RubenArthuys <ruben.arthuys@gmail.com>
2020-09-25 10:32:00 +0200

    Updates

---
+++ precache-manifest.c3ec061786c15e43181e7c8b26227843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e21680ef90bdd955e185cad0daf48e9",
+    "url": "/names-list/index.html"
+  },
+  {
+    "revision": "1fd26e42f0baf814414b",
+    "url": "/names-list/static/css/main.7b3b0210.chunk.css"
+  },
+  {
+    "revision": "f58e999a1c81baed0312",
+    "url": "/names-list/static/js/2.0de4caa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/names-list/static/js/2.0de4caa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd26e42f0baf814414b",
+    "url": "/names-list/static/js/main.e8a2de2b.chunk.js"
+  },
+  {
+    "revision": "31df9ce0b12a4fb18ede",
+    "url": "/names-list/static/js/runtime-main.f10602bd.js"
+  }
+]);
